e8f719e4b4d91b56480d0109d1153cbc8ea9f72b
TunmiDev <adewoleoluwatunmise@gmail.com>
2024-12-08 18:47:09 +0100

    Made it fully responsive

--- src/Components/AdviceGenLine/AdviceGenLine.jsx
+++ src/Components/AdviceGenLine/AdviceGenLine.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./AdviceGenLine.css";
 import img from "../../assets/images/pattern-divider-desktop.svg";
 
 const AdviceGenLine = () => {
   return (
-    <div>
+    <div className="line">
       <img src={img} alt="" />
     </div>
   );
